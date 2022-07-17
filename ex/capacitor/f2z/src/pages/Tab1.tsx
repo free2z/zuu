13609ad63eedfef0380e85b35713ac7a07e8bf4b
Skylar Saveland <skylar.saveland@gmail.com>
2022-07-17 15:00:22 -0700

    WIP ex/capacitor, playing with ionic

--- ex/capacitor/f2z/src/pages/Tab1.tsx
+++ ex/capacitor/f2z/src/pages/Tab1.tsx
@@ -1,4 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonAvatar, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { AccountSwitcher } from '../components/AccountSwitcher';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -8,6 +9,7 @@ const Tab1: React.FC = () => {
       <IonHeader>
         <IonToolbar>
           <IonTitle>Tab 1</IonTitle>
+          <AccountSwitcher />
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
