45fc19a47e5c81b299f1ecf2e9df99de7bb6141d
Skylar Saveland <skylar.saveland@gmail.com>
2022-09-24 22:43:18 -0700

    STSTIC_ROOT

--- ex/websites/timelines/dj/dj/settings.py
+++ ex/websites/timelines/dj/dj/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = f"{BASE_DIR}/static"
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
