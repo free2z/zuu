012aa2b0fa7c45acc8c76891d56787f6287993f4
Skylar Saveland <skylar.saveland@gmail.com>
2022-09-24 23:21:00 -0700

    os

--- ex/websites/timelines/dj/dj/settings.py
+++ ex/websites/timelines/dj/dj/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 from django.core.management.utils import get_random_secret_key
