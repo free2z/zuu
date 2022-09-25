1bdf2ec2d4ac2805e76009067ac7f7a9b452a396
Skylar Saveland <skylar.saveland@gmail.com>
2022-09-24 23:19:25 -0700

    some settings

--- ex/websites/timelines/dj/dj/settings.py
+++ ex/websites/timelines/dj/dj/settings.py
@@ -12,22 +12,25 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+from django.core.management.utils import get_random_secret_key
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_ROOT = f"{BASE_DIR}/static"
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m)371f)$ho=ir=io$0myp=_uqxfxp5i5y%i=%onynteyr59(g%'
+SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "timelines.azhr.io",
+]
 
 
 # Application definition
