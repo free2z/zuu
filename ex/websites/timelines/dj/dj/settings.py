89406ed63be65c522f1c5823731efac183e221a9
Skylar Saveland <skylar.saveland@gmail.com>
2022-09-24 23:28:41 -0700

    debug=False

--- ex/websites/timelines/dj/dj/settings.py
+++ ex/websites/timelines/dj/dj/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "timelines.azhr.io",
