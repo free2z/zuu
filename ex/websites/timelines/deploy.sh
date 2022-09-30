dd6b6f2100526f16a9c426949561ca91d68bfc39
Azhr Ciel <azhr.ciel@gmail.com>
2022-09-30 12:27:10 -0700

    add grappelli to django-timelines

--- ex/websites/timelines/deploy.sh
+++ ex/websites/timelines/deploy.sh
@@ -4,7 +4,9 @@
 # git pull
 source env/bin/activate
 pushd dj
+export DJANGO_SETTINGS_MODULE="dj.prod_settings"
 ./manage.py migrate
+./manage.py collectstatic
 sudo systemctl daemon-reload
 sudo systemctl enable gunicorn.socket
 sudo systemctl restart gunicorn.socket
--- ex/websites/timelines/dj/dj/urls.py
+++ ex/websites/timelines/dj/dj/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from timelines import views as timelines_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('grappelli/', include('grappelli.urls')),
 
     path('<str:timeline_slug>/', timelines_views.detail, name='timelines_detail'),
 ]
