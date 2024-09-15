8f22a2cc65950601c233e810aefbddd47f0cd7dd
Skylar Saveland <skylar.saveland@gmail.com>
2024-09-14 18:30:50 -0700

    update submodules (#88)
    * update submodules
    
    * update submodules script
    
    * ..

---
+++ update_submodules.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Iterate through each submodule defined in .gitmodules
+git config -f .gitmodules --get-regexp '^submodule\..*\.path$' | while read path_key path
+do
+    # Get the branch name for this submodule
+    branch_key=$(echo $path_key | sed 's/\.path$/.branch/')
+    branch=$(git config -f .gitmodules --get "$branch_key")
+
+    # Default to 'master' if no branch is defined
+    branch=${branch:-master}
+
+    echo "Updating submodule at path: $path"
+    echo "Branch specified in .gitmodules: $branch"
+
+    # Change to the submodule directory
+    if [ -d "$path" ]; then
+        (cd $path &&
+            echo "Fetching latest changes from origin in submodule $path..." &&
+            git fetch origin &&
+            echo "Checking out branch $branch in submodule $path..." &&
+            git checkout $branch &&
+            echo "Pulling latest changes for branch $branch in submodule $path..." &&
+            git pull origin $branch) || {
+                echo "Error updating submodule $path on branch $branch"
+            }
+    else
+        echo "Directory $path does not exist. Skipping..."
+    fi
+
+    echo "-----------------------------------------"
+done
