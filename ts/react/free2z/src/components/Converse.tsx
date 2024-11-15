457bbb71c187030597d33795f34f7442f62eaff1
Kevin Bravo <bravokevinto@gmail.com>
2024-11-15 05:45:44 -0400

    fix(ts/react/free2z): render 404 when converse comment is not found

--- ts/react/free2z/src/components/Converse.tsx
+++ ts/react/free2z/src/components/Converse.tsx
@@ -1,44 +1,45 @@
 import { useParams } from "react-router-dom";
 import { CommentData } from "./DisplayThreadedComments";
-import { useEffect, useState } from "react";
+import { lazy, useEffect, useState } from "react";
 import axios from "axios";
 import { Grid } from "@mui/material";
 import DisplayThreadedComment from "./DisplayThreadedComment";
-
+const Global404 = lazy(() => import('../Global404'))
 
 export default function Converse() {
-    const params = useParams()
-    const parent = params.commentUUID
-
-    const [comment, setComment] = useState({} as CommentData)
+    const params = useParams();
+    const parent = params.commentUUID;
+    const [isLoading, setLoading] = useState(true)
+    const [comment, setComment] = useState<CommentData>({} as CommentData);
 
     useEffect(() => {
         axios.get(`/api/comments/${parent}/`).then(res => {
-            // console.log("COMMENT", res)
             setComment(res.data)
+        }).catch(() => {
+            // prevent error when comment is not found
+        }).finally(() => {
+            setLoading(false);
         })
     }, [parent])
 
-    if (!comment.uuid) {
-        return null
-    }
-
-    return (
-        <Grid
-            textAlign="left"
-            alignItems="center"
-            justifyContent="center"
-            width="100%"
-            style={{
-                overflowWrap: "break-word",
-                overflowX: "auto",
-                padding: "0.33em",
-            }}
-        >
-            <DisplayThreadedComment
-                comment={comment}
-                top={true}
-            />
-        </Grid>
-    )
+    if (isLoading) return null
+    if (!comment.uuid) return <Global404 />
+        return (
+            <Grid
+                textAlign="left"
+                alignItems="center"
+                justifyContent="center"
+                width="100%"
+                style={{
+                    overflowWrap: "break-word",
+                    overflowX: "auto",
+                    padding: "0.33em",
+                }}
+            >
+                <DisplayThreadedComment
+                    comment={comment}
+                    top={true}
+                />
+            </Grid>
+        )
 }
